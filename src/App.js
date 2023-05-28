7247a7f63a9f238a95f2a132b3d819c8541c4b77
Raquel Barrio <r.barrio.f@gmail.com>
2023-05-28 21:05:38 +0200

    done

--- src/App.js
+++ src/App.js
@@ -28,35 +28,43 @@ function App() {
     setContacts(sortByPopularity)
   }
 
+  function deleteContact(contactId) {
+    let afterDeleteContacts = contacts.filter(contactToDelete => contactToDelete.id !== contactId)
+    setContacts(afterDeleteContacts)
+  }
+
 
   return <div className="App">
+  <div className='title'>
   <h1>IronContacts</h1>
-  {remainingContacts.length > 0 ? <button onClick={addRandomContact}>Add Random Contact</button> : ""}
-  <br/>
+  {remainingContacts.length > 0 ? <button style={{"width": "100px", "margin-left": "auto", "margin-right": "auto"}} onClick={addRandomContact}>Add Random Contact</button> : ""}
+  <div>
   <button onClick={sortByName}>Sort by Name</button>
-  <button onClick={sortByPopularity}>Sort by Popularity</button> 
-    <table>
+  <button onClick={sortByPopularity}>Sort by Popularity</button>
+  </div>
+  </div>
+    <table style={{"margin-left": "auto", "margin-right": "auto"}}>
     <tr>
       <th>Picture</th>
-      <th>Name</th> {/* <--iteration 4 */}
-      <th>Popularity</th> {/* <--iteration 4 */}
-      <th>Won an Oscar</th>
-      <th>Won an Emmy</th>
+      <th>Name</th>
+      <th>Popularity</th>
+      <th><p>Won</p><p>Oscar</p></th>
+      <th><p>Won</p><p>Emmy</p></th>
+      <th>Actions</th>
     </tr>
     {contacts.map(contact=>{
       let hasOscar = contact.wonOscar ? <img src={require("./images/trophy.png")} className="award" alt='trophy'/> : ""
       let hasEmmy = contact.wonEmmy ? <img src={require("./images/trophy.png")} className="award" alt='trophy'/> : ""
-
       return <tr key={contact.id}>
     <td><img src={contact.pictureUrl} alt='Contact'/></td>
     <td>{contact.name}</td>
     <td>{contact.popularity}</td>
     <td>{hasOscar}</td>
     <td>{hasEmmy}</td>
+    <td><button onClick={() => deleteContact(contact.id)}>Delete</button></td>
   </tr>
     })}
-    </table>
-    
+    </table>    
   </div>;
 }
 
